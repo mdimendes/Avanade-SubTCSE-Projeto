1b93ffac241e6298348f453d0f1eb1911724b7a7
Marcos Mendes <marcos.diego.mendes@avanade.com>
2021-06-17 08:59:26 -0300

    aula 4

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,20 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public class EmployeeRole
+    {
+        public string id { get; set; }
+
+        public string RoleName { get; set; }
+
+        public EmployeeRole(string id, string roleName)
+        {
+            this.id = id;
+            RoleName = roleName;
+        }
+
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -0,0 +1,13 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
+using System;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
+{
+    public class EmployeeRoleService : IEmployeeRoleService
+    {
+        public void AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        {
+            throw new NotImplementedException("Calma o coração");
+        }
+    }
+}
